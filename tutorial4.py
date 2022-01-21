1c38aa353e806083a3cd76d6027d8605c759b9eb
BM coding for kids <88277732+BenjaminM-10@users.noreply.github.com>
2022-01-21 13:39:03 +0200

    Create tutorial4.py

---
+++ tutorial4.py
@@ -0,0 +1,39 @@
+# Data types (values)
+
+
+# integer is a whole number that can be positive or negative for example(10,-54,2...)
+
+# float is a number with a decimal point (it can also be positive or negative)for example(3.45,-2.8,1.0...)
+
+# string is text for example ('Hello world')
+
+# boolean is either True or False
+
+
+# code examples
+integer = 56
+float_ = 54.5
+string = 'Hello world' # text must be placed between quotes
+boolean = True #or False
+
+#get the value of an element using the type() function
+
+print(type(integer))
+print(type(float_))
+print(type(string))
+print(type(boolean))
+
+
+# convert a data type to another data type
+# example
+
+
+print(bool(1)) # will convert the number 1 to a True (0 = False, 1 = True)
+
+print(int(1.0)) # will convert the number 1.0 to an integer by removing the decimal point
+
+print(float(5)) # will convert the number 5 to a float by adding to the number 5 a decimal point
+
+print(str(67)) # converts the numbers value to text
+
+
