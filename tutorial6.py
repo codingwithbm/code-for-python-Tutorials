061eb79bacdc93f0336a22de7648eae09deb91cc
BM coding for kids <88277732+BMcodingforkids@users.noreply.github.com>
2022-04-11 17:41:48 +0300

    Add files via upload

---
+++ tutorial6.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter a number "))
+num2 = int(input("Enter another number "))
+
+if num1 > num2:
+    print("num1 is greater then num2")
+elif num1 < num2:
+    print("num2 is greater then num1")
+elif num1 == num2:
+    print("They are equal")
+else:
+    print("Error")
+    
