230824809b365549320409f56915431f599e0202
BM coding for kids <88277732+BMcodingforkids@users.noreply.github.com>
2022-04-14 17:05:59 +0300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,12 @@
+#list
+shopping = ["eggs","tomatoes","milk","orange juice"]
+
+
+#check the amount of items in a list
+shopping = ["eggs","tomatoes","milk","orange juice"]
+print(len(shopping))
+
+#add an item to the list without changing the list
+shopping = ["eggs","tomatoes","milk","orange juice"]
+shopping.append("meat")
+print(shopping)
