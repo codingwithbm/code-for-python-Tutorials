fe6efe40afa57ebe8c80ada74ffe6f1a1fb6d707
BM coding for kids <88277732+BMcodingforkids@users.noreply.github.com>
2022-06-01 19:56:31 +0300

    Add files via upload

---
+++ python turtle/tutorial1/main.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+bgcolor("#a6a6a6")
+color("blue")
+
+#bottom
+right(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+
+#top
+right(120)
+forward(100)
+right(120)
+forward(100)
+
+
+
+
+
+done()
