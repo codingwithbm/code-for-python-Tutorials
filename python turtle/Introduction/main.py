b609ca385d4c27231ab96530bab9352e2e12200a
BM coding for kids <88277732+BMcodingforkids@users.noreply.github.com>
2022-06-13 17:37:02 +0300

    Add files via upload

---
+++ python turtle/Introduction/main.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+#color
+color("blue")
+
+#bottom
+right(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+#top
+right(120)
+forward(100)
+right(120)
+forward(100)
+
+
+done()
---
+++ python turtle/Pen Up:Down/main.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+pensize(3)
+
+for i in range(5):
+    color("#fff200")
+    forward(100)
+    right(144)
+
+penup()
+forward(150)
+pendown()
+
+for i in range(5):
+    color("#fff200")
+    forward(100)
+    right(144)
+
+
+done()
