09cb9e90a396a5999d187960e8a4fa12361ea63b
BM coding for kids <88277732+BMcodingforkids@users.noreply.github.com>
2022-08-30 18:37:47 +0300

    Update code.py

--- python turtle/begin:end fill/code.py
+++ python turtle/begin:end fill/code.py
@@ -2,12 +2,16 @@ from turtle import *
 color("black")
 begin_fill()
 
+#stripe1
+
 for i in range(2):
 	forward(200)
 	left(90)
 	forward(40)
 	left(90)
 end_fill()
+
+#stripe2
 
 begin_fill()
 
@@ -22,6 +26,9 @@ for i in range(2):
 	left(90)
 end_fill()
 
+
+#stripe3
+
 begin_fill()
 
 right(90)
@@ -34,9 +41,6 @@ for i in range(2):
 	forward(40)
 	left(90)
 end_fill()
-
-
-
 
 
 
