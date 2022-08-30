4f9b26a07c8f2e081c02b05f93fc686ac29c6560
BM coding for kids <88277732+BMcodingforkids@users.noreply.github.com>
2022-08-30 18:36:30 +0300

    Add files via upload

---
+++ python turtle/begin:end fill/code.py
@@ -0,0 +1,46 @@
+from turtle import *
+color("black")
+begin_fill()
+
+for i in range(2):
+	forward(200)
+	left(90)
+	forward(40)
+	left(90)
+end_fill()
+
+begin_fill()
+
+right(90)
+forward(40)
+left(90)
+color("red")
+for i in range(2):
+	forward(200)
+	left(90)
+	forward(40)
+	left(90)
+end_fill()
+
+begin_fill()
+
+right(90)
+forward(40)
+left(90)
+color("gold")
+for i in range(2):
+	forward(200)
+	left(90)
+	forward(40)
+	left(90)
+end_fill()
+
+
+
+
+
+
+
+
+
+done()
